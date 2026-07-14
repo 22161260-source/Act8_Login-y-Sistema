0131d10b6e3f9186a9e8173f85d54b8a0d6ae7ae
22161260-source <22161260@itoaxaca.edu.mx>
2026-07-14 07:50:02 -0600

    Clean up vite.config.js and update base path
    Remove comments and update base path placeholder

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Fase 4: sustituye XX por el número de tu equipo antes de "npm run build".
-  // Ejemplo, equipo 05 -> base: '/t3_act8_eq05/'
   base: '/t3_act8_eqXX/',
 })
